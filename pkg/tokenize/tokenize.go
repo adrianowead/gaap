1bca31b285bf5d172b001bf537108a7a905ffd0a
Adriano Maciel <adriano_mail@hotmail.com>
2018-10-27 16:29:15 -0300

    Add function to convert string to bytes

--- pkg/tokenize/tokenize.go
+++ pkg/tokenize/tokenize.go
@@ -7,7 +7,12 @@ import (
 
 // TextToSentence given a string and returns separeted senteces
 func TextToSentence(text string) []string {
-	var sentences = sentence.SentenceTokenizer(text)
+	b := StringToByte(text)
 
 	return sentences
+}
+
+// StringToByte converts any string do byte slices
+func StringToByte(text string) []byte {
+	return []byte(text)
 }
