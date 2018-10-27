03c3881bdc57df0a89cbf3f9677188ba786c298b
Adriano Maciel <adriano_mail@hotmail.com>
2018-10-27 16:32:25 -0300

    Add tests on main package

--- cmd/gaap/main.go
+++ cmd/gaap/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main(){
-	//
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"../../pkg/tokenize"
+)
+
+func main() {
+
+	absPath, _ := filepath.Abs("testdata/lero-lero.txt")
+
+	text, err := ioutil.ReadFile(absPath)
+
+	check(err)
+
+	testSentence(string(text))
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func testSentence(text string) {
+	var tokens = tokenize.TextToSentence(text)
+
+	// fmt.Println(tokens[0])
+	fmt.Println(len(tokens))
 }
