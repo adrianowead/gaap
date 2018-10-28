bf358e2933a33b990e78f5a37d32ab0b3cc192cd
Adriano Maciel <adriano_mail@hotmail.com>
2018-10-27 21:49:10 -0300

    Add normalize string function, and text to word function

--- pkg/tokenize/tokenize.go
+++ pkg/tokenize/tokenize.go
@@ -5,18 +5,30 @@ import (
 	"bytes"
 
 	"../sentence"
+	"../word"
 )
 
 // TextToSentence given a string and returns separeted senteces
 func TextToSentence(text string) []string {
-	b := StringToByte(text)
-	b = NormalizeNewLine(b)
-
-	text = ByteToString(b)
+	text = normalizeString(text)
 
 	var sentences = sentence.Tokenizer(text)
 
 	return sentences
+}
+
+func normalizeString(text string) string {
+	b := StringToByte(text)
+	b = NormalizeNewLine(b)
+
+	return ByteToString(b)
+}
+
+// TextToWord receives a string and return words
+func TextToWord(text string) []string {
+	text = normalizeString(text)
+
+	return word.TextToWord(text)
 }
 
 // StringToByte converts any string do byte slices
