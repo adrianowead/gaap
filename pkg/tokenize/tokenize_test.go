8cf11bf7a1efa6af79e829175eb4ae4ea5d63221
Adriano Maciel <adriano_mail@hotmail.com>
2018-10-27 21:49:29 -0300

    Add test to text to word, and benchmark

--- pkg/tokenize/tokenize_test.go
+++ pkg/tokenize/tokenize_test.go
@@ -137,3 +137,31 @@ func BenchmarkNormalizeNewLine(b *testing.B) {
 		NormalizeNewLine(test)
 	}
 }
+
+func TestTextToWord(t *testing.T) {
+	test := "gaap é o melhor software em GoLang!"
+
+	want := []string{"gaap", "é", "o", "melhor", "software", "em", "GoLang!"}
+
+	result := TextToWord(test)
+
+	if len(result) != len(want) {
+		t.Errorf("Length bytes results '%d', '%d' received", len(result), len(want))
+	}
+
+	for idx, text := range want {
+		if text != result[idx] {
+			t.Errorf("Position '%d' resturns ('%s'), expected ('%s')", idx, result[idx], text)
+		}
+	}
+}
+
+func BenchmarkTextToWord(b *testing.B) {
+	test := "gaap é o melhor software em GoLang!"
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		TextToWord(test)
+	}
+}
