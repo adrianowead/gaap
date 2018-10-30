c0fabab77cfb5fb83963a96b3ba810b76e9ac358
Adriano Maciel <adriano_mail@hotmail.com>
2018-10-30 15:58:38 -0300

    Update rules acrostic to parse sub commands

--- pkg/acrostic/rules.go
+++ pkg/acrostic/rules.go
@@ -2,47 +2,62 @@ package acrostic
 
 import (
 	"regexp"
+	"strings"
 )
 
 // Rules define the structure of current expression
 type Rules struct {
-	object string // {}
-	count  int    // ||
-	each   int    // []
-	cmd    string // ^ $ #
-	exp    string // ""
+	Object string // {}
+	Count  int    // ||
+	Each   int    // []
+	Cmd    string // ^ $ #
+	Exp    string // ""
+	Sub    *Rules
 }
 
 var basicStructure = "{}||[]"
 var validCommands = []string{`^`, `$`, `#`}
 
 // ParseRules process the received expression and returns Rules struct
-func ParseRules(rules string) (bool, Rules) {
-	out := Rules{}
+func ParseRules(rules string, out *Rules) bool {
+	valid := HaveValidStructure(rules, out)
+	out.Exp = extractEspression(rules, out.Cmd)
 
-	valid := HaveValidStructure(rules, &out)
+	// if have subcommand
+	if valid && out.Cmd == "#" && len(out.Exp) > 0 {
+		out.Sub = &Rules{}
 
-	return valid, out
+		out.Exp = out.Exp[1:]
+		out.Exp = out.Exp[:len(out.Exp)-1]
+
+		_, out.Sub = parseSubCommand(out.Exp)
+	}
+
+	return valid
 }
 
 // HaveValidStructure validate if expression have minimum structure
 func HaveValidStructure(rules string, r *Rules) bool {
 
-	haveBasic := haveBasicStruct(rules, r)
+	haveBasic := haveBasicStruct(rules)
 	haveExp := haveBasicExp(rules, r)
 
 	return haveBasic && haveExp
 }
 
 func extractStruct(rules string) string {
-	re := regexp.MustCompile("[0-9a-zA-Z]")
+	re := regexp.MustCompile(`[^[]{}|^#$]`)
 
 	exp := re.ReplaceAllString(rules, "")
+
+	re = regexp.MustCompile(`[0-9a-zA-Z:%]`)
+
+	exp = re.ReplaceAllString(exp, "")
 
 	return exp
 }
 
-func haveBasicStruct(rules string, r *Rules) bool {
+func haveBasicStruct(rules string) bool {
 	exp := extractStruct(rules)
 
 	exp = exp[:len(basicStructure)]
@@ -53,15 +68,35 @@ func haveBasicStruct(rules string, r *Rules) bool {
 func haveBasicExp(rules string, r *Rules) bool {
 	exp := extractStruct(rules)
 
-	r.cmd = exp[len(basicStructure):][:1]
+	r.Cmd = exp[len(basicStructure):][:1]
 
 	valid := false
 
 	for _, item := range validCommands {
-		if item == r.cmd {
+		if item == r.Cmd {
 			valid = true
 		}
 	}
 
 	return valid
+}
+
+func extractEspression(rules string, cmd string) string {
+	out := ""
+
+	i := strings.Index(rules, cmd)
+
+	if i > -1 {
+		out = rules[i+1:]
+	}
+
+	return out
+}
+
+func parseSubCommand(sub string) (bool, *Rules) {
+	r := &Rules{}
+
+	valid := ParseRules(sub, r)
+
+	return valid, r
 }
