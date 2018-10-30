93370628b41ea30d104f8137e824b9f211209d47
Adriano Maciel <adriano_mail@hotmail.com>
2018-10-30 15:58:59 -0300

    Adjust output rules validate

--- pkg/acrostic/acrostic.go
+++ pkg/acrostic/acrostic.go
@@ -3,7 +3,9 @@ package acrostic
 // IsValidRules returns true or false, if current rule is valid
 // to acrostic method
 func IsValidRules(rule string) (bool, Rules) {
-	valid, basicStruct := ParseRules(rule)
+	r := Rules{}
 
-	return valid, basicStruct
+	valid := ParseRules(rule, &r)
+
+	return valid, r
 }
